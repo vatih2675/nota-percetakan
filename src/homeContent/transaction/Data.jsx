c46ec39a1fb2228e2cb1f304ab609dce92ab9c7f
vatih2675 <ruslansamuel11@gmail.com>
2025-05-15 00:31:08 +0800

    Edit view in print mode

--- src/homeContent/transaction/Data.jsx
+++ src/homeContent/transaction/Data.jsx
@@ -57,7 +57,7 @@ export default function Data({dataNota, tanggalHariIni}){
                 }`}
                 onClick={closeNota}
               >
-                <div className="relative w-full sm:w-[107.5mm] h-full sm:h-[148.5mm] flex flex-col justify-between border border-gray-200 mx-auto p-10 sm:p-5 bg-white shadow-md">
+                <div className="relative w-[107.5mm] h-[148.5mm] flex flex-col justify-between sm:border border-gray-200 mx-auto p-5 bg-white sm:shadow-md">
                   <div className="flex justify-between items-start">
                     <img
                       src="images/logo-percetakan.png"
@@ -96,24 +96,24 @@ export default function Data({dataNota, tanggalHariIni}){
                   <img
                     src="images/logo.png"
                     alt="Logo UBMG"
-                    className="absolute w-70 sm:w-50 top-28 sm:top-20 left-0 translate-1/2 bg-transparent z-0"
+                    className="absolute w-50 top-20 left-0 translate-1/2 bg-transparent z-0"
                   />
-                  <table className="relative w-full sm:text-xs bg-white/95 z-1">
+                  <table className="relative w-full text-xs bg-white/95 z-1">
                     <thead>
                       <tr className="bg-sky-500 text-white">
-                        <th className="p-2 sm:p-1 border border-gray-300 text-center w-[8%]">
+                        <th className="p-1 border border-gray-300 text-center w-[8%]">
                           NO
                         </th>
-                        <th className="p-2 sm:p-1 border border-gray-300 text-center">
+                        <th className="p-1 border border-gray-300 text-center">
                           URAIAN
                         </th>
-                        <th className="p-2 sm:p-1 border border-gray-300 text-center">
+                        <th className="p-1 border border-gray-300 text-center">
                           JUMLAH
                         </th>
-                        <th className="p-2 sm:p-1 border border-gray-300 text-center">
+                        <th className="p-1 border border-gray-300 text-center">
                           HARGA
                         </th>
-                        <th className="p-2 sm:p-1 border border-gray-300 text-center">
+                        <th className="p-1 border border-gray-300 text-center">
                           TOTAL
                         </th>
                       </tr>
@@ -121,19 +121,19 @@ export default function Data({dataNota, tanggalHariIni}){
                     <tbody>
                       {nt.data.map((dt, index) => (
                         <tr key={index} className="text-gray-600">
-                          <td className="p-2 sm:p-1 border border-gray-300 text-center">
+                          <td className="p-1 border border-gray-300 text-center">
                             {index + 1}
                           </td>
-                          <td className="p-2 sm:p-1 border border-gray-300 text-start">
+                          <td className="p-1 border border-gray-300 text-start">
                             {dt.uraian}
                           </td>
-                          <td className="p-2 sm:p-1 border border-gray-300 text-center">
+                          <td className="p-1 border border-gray-300 text-center">
                             {angka(dt.jumlah)} {dt.satuan}
                           </td>
-                          <td className="p-2 sm:p-1 border border-gray-300 text-end">
+                          <td className="p-1 border border-gray-300 text-end">
                             {angka(dt.harga)}
                           </td>
-                          <td className="p-2 sm:p-1 border border-gray-300 text-end">
+                          <td className="p-1 border border-gray-300 text-end">
                             {angka(dt.harga * dt.jumlah)}
                           </td>
                         </tr>
@@ -143,19 +143,19 @@ export default function Data({dataNota, tanggalHariIni}){
                         for (let i = index; i < 9; i++) {
                           trKosong.push(
                             <tr key={i}>
-                              <th className="p-2 sm:p-1 border border-gray-300 text-white">
+                              <th className="p-1 border border-gray-300 text-white">
                                 .
                               </th>
-                              <th className="p-2 sm:p-1 border border-gray-300 text-white">
+                              <th className="p-1 border border-gray-300 text-white">
                                 .
                               </th>
-                              <th className="p-2 sm:p-1 border border-gray-300 text-white">
+                              <th className="p-1 border border-gray-300 text-white">
                                 .
                               </th>
-                              <th className="p-2 sm:p-1 border border-gray-300 text-white">
+                              <th className="p-1 border border-gray-300 text-white">
                                 .
                               </th>
-                              <th className="p-2 sm:p-1 border border-gray-300 text-white">
+                              <th className="p-1 border border-gray-300 text-white">
                                 .
                               </th>
                             </tr>
@@ -166,22 +166,22 @@ export default function Data({dataNota, tanggalHariIni}){
                     </tbody>
                     <tfoot>
                       <tr className="text-sky-700">
-                        <th className="p-2 sm:p-1" colSpan="4"></th>
-                        <th className="p-2 sm:p-1 border border-gray-300 text-end">
+                        <th className="p-1" colSpan="4"></th>
+                        <th className="p-1 border border-gray-300 text-end">
                           {angka(nt.total)}
                         </th>
                       </tr>
                     </tfoot>
                   </table>
                   <div className="flex justify-center items-center">
-                    <div className="w-8/12 sm:w-7/12 pe-10 sm:pe-0 text-xs text-sky-700">
+                    <div className="w-7/12 pe-0 text-xs text-sky-700">
                       <p className="mb-2">Terima kasih atas kunjungan Anda.</p>
                       <p>
                         Barang yang sudah dibeli tidak dapat
                         dikembalikan/ditukar, kecuali ada perjanjian.
                       </p>
                     </div>
-                    <div className="w-4/12 sm:w-5/12 text-sm text-center text-sky-700">
+                    <div className="w-5/12 text-sm text-center text-sky-700">
                       <p className="mb-8">Operator</p>
                       <p className="font-bold underline">RUSLAN SAMUEL</p>
                     </div>
